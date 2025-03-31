6465a0b6929db042c530d8e1c466dc95b7fcde7b
Murali Dhanakoti <dhanakoti.murali@gmail.com>
2025-03-31 13:39:44 -0700

    Script to download and make metal-cpp available

---
+++ setupMetalCpp.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -ex
+
+buildDir="$1"
+
+if [[ ! -d "$buildDir/_deps/downloads" ]]; then
+   mkdir -p "$buildDir/_deps/downloads"
+fi 
+
+if [[ ! -f "$buildDir/_deps/downloads/metal-cpp_macOS15.2_iOS18.2.zip" ]]; then
+    pushd "$buildDir/_deps/downloads/"
+    wget https://developer.apple.com/metal/cpp/files/metal-cpp_macOS15.2_iOS18.2.zip
+    popd 
+fi
+
+if [[ ! -d "$buildDir/_deps/metal-cpp" ]]; then
+    pushd "$buildDir/_deps"
+    unzip downloads/metal-cpp_macOS15.2_iOS18.2.zip
+    popd
+fi
